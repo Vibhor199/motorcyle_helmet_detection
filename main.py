fcd4239c869f4ba35fa97abb1cdee627d50ca7fc
Vibhor197 <vibhorsharma197@gmail.com>
2020-03-09 23:07:24 +0530

    a balanced working svm model but only 70% accuracy

--- main.py
+++
@@ -1,5 +0,0 @@
-from moving_object_segmentation.mog2_based_segmentation import detect_motion_in_youtube
-
-
-detect_motion_in_youtube('https://www.youtube.com/watch?v=JtGyMlFpszk')
-
--- moving_object_segmentation/mog2_based_segmentation.py
+++ moving_object_segmentation/mog2_based_segmentation.py
@@ -4,10 +4,10 @@ import pafy
 
 
 def detect_motion_in_youtube(url: str):
-    vPafy = pafy.new(url)
-    play = vPafy.getbestvideo(preftype="webm")
+    v_pafy = pafy.new(url)
+    play = v_pafy.getbestvideo(preftype="webm")
 
-    backSub = cv.createBackgroundSubtractorMOG2(history = 2,varThreshold = 16, detectShadows = True)
+    background_subtract = cv.createBackgroundSubtractorMOG2(history = 2,varThreshold = 16, detectShadows = True)
     # backSub = cv.createBackgroundSubtractorKNN()
 
     capture = cv.VideoCapture(play.url)
@@ -15,16 +15,16 @@ def detect_motion_in_youtube(url: str):
 
     while True:
         ret, frame = capture.read()
-        img = cv.GaussianBlur(frame,(5,5),0)
+        img = cv.GaussianBlur(frame, (5, 5), 0)
         # frame = cv.flip(frame   , 1)
 
-        fgMask = backSub.apply(img)
-        kernel = np.ones((11,11),np.uint8)
-        fgMask = cv.dilate(fgMask,kernel,iterations = 1)
-        fgMask = cv.morphologyEx(fgMask, cv.MORPH_CLOSE, kernel)
-        fgMask = cv.morphologyEx(fgMask, cv.MORPH_OPEN, kernel)
+        fg_mask = background_subtract.apply(img)
+        kernel = np.ones((11, 11), np.uint8)
+        fg_mask = cv.dilate(fg_mask, kernel, iterations = 1)
+        fg_mask = cv.morphologyEx(fg_mask, cv.MORPH_CLOSE, kernel)
+        fg_mask = cv.morphologyEx(fg_mask, cv.MORPH_OPEN, kernel)
 
-        res = cv.bitwise_and(frame,frame,mask = fgMask)
+        res = cv.bitwise_and(frame, frame, mask=fg_mask)
 
         cv.imshow('Frame', frame)
         cv.imshow('FG Mask', res)
